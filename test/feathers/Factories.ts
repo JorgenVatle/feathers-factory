a0fa10473bd4c39173d461a26b222fda0ba728e7
Jørgen Vatle <Jorgen@WcAServices.net>
2022-04-25 16:10:43 +0200

    Add factory for comments on own article.

--- test/feathers/Factories.ts
+++ test/feathers/Factories.ts
@@ -4,6 +4,7 @@ import App from './App';
 
 const UsersService = App.service('users');
 const ArticlesService = App.service('articles');
+const CommentService = App.service('comments');
 
 export const UserFactory = new Factory(UsersService, {
     id: () => process.hrtime().join('-'),
@@ -15,10 +16,26 @@ export const ArticleFactory = new Factory(ArticlesService, {
     userId: async () => {
         const user = await UserFactory.create();
         return user.id;
-    }
+    },
 });
 
 export const StrictArticleFactory = new Factory<{ userId: string, content: string }>(ArticlesService, {
     userId: () => 'some-string',
     content: () => 'foobar',
-})
+});
+
+export const CommentOnOwnArticleFactory = new Factory<{
+    userId: string;
+    articleId: string;
+    content: string;
+}>(CommentService, {
+    articleId: async () => {
+        const article = await ArticleFactory.create();
+        return article.id;
+    },
+    userId: async function() {
+        const user = UsersService.get(this.articleId);
+        return user.id;
+    },
+    content: Faker.lorem.paragraph,
+});
