666f79048616cfbe97cdc20e88291c4eed11bd2d
Jørgen Vatle <Jorgen@WcAServices.net>
2022-04-25 16:15:18 +0200

    Assert that we can create self-referencing documents.

--- test/implementations/cross-dependent-factories.spec.ts
+++ test/implementations/cross-dependent-factories.spec.ts
@@ -1,6 +1,6 @@
 import Expect from 'expect';
 import App from '../feathers/App';
-import { ArticleFactory } from '../feathers/Factories';
+import { ArticleFactory, CommentOnOwnArticleFactory } from '../feathers/Factories';
 
 describe('relational/cross-dependent factories', () => {
     it('creates user documents for articles', async () => {
@@ -11,5 +11,15 @@ describe('relational/cross-dependent factories', () => {
         const user = await App.service('users').get(article.userId);
 
         Expect(user).toHaveProperty('id', article.userId);
-    })
+    });
+
+    it('can create articles with comments from the author', async () => {
+        const comment = await CommentOnOwnArticleFactory.create();
+        const user = await App.service('users').get(comment.userId);
+        const article = await App.service('comments').get(comment.articleId);
+
+        Expect(comment).toBeTruthy();
+        Expect(user).toHaveProperty('id', comment.userId);
+        Expect(article).toHaveProperty('id', comment.articleId);
+    });
 })
