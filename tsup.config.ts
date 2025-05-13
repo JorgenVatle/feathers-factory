c85860a4c42b7064365610a97691839ff0766b78
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-13 22:15:00 +0300

    Compile for ESM and CJS

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     entry: ['src/index.ts'],
     skipNodeModulesBundle: true,
+    format: ['cjs', 'esm'],
     sourcemap: true,
     dts: true,
 })
