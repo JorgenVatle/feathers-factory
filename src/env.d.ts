f7b0a962a1503587a74a51e030245706beade89f
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-14 15:07:50 +0300

    Add declaration for globals argument

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,5 @@
 declare module 'clues' {
     type Solver = string | ((...params: any) => void);
     
-    export default function Clues<T>(data: T, key: Solver | Solver[]): any;
+    export default function Clues<T>(data: T, key: Solver | Solver[], globals?: object): any;
 }
