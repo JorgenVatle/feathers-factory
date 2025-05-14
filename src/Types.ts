aa046135507f3ff47e487d8f62f5046c29be111e
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-14 04:06:09 +0300

    Add type alias for service classes compatible with Feathers Factory

--- src/Types.ts
+++ src/Types.ts
@@ -1,6 +1,17 @@
 import { Params } from '@feathersjs/feathers';
 
 
+/**
+ * Utility type for any Feathers or class that implements
+ * an interface compatible with Feathers Factory.
+ * @param TSchema The expected input type for the service create() method.
+ * @param TResult The return type for the provided service class
+ */
+export type FactoryService<
+    TSchema = unknown,
+    TResult = TSchema,
+> = FactoryCompatibleService<TSchema, TResult>
+
 export interface FactoryCompatibleService<
     TSchema,
     TResult = TSchema,
