02cd40328ce602eb70742b2e23121130742ded2b
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-14 07:05:05 +0300

    Update nesting for generator function tests

--- test/Factory.spec.ts
+++ test/Factory.spec.ts
@@ -1,5 +1,5 @@
 import { faker, simpleFaker } from '@faker-js/faker';
-import { afterEach, describe, expect, it, vi } from 'vitest';
+import { afterAll, afterEach, describe, expect, it, vi } from 'vitest';
 import { Factory, type FactoryService } from '../src';
 
 describe('Factory', () => {
@@ -18,43 +18,49 @@ describe('Factory', () => {
         },
     });
     
-    const _id = vi.spyOn(simpleFaker.string, 'uuid');
-    const firstName = vi.spyOn(faker.person, 'firstName');
-    const lastName = vi.spyOn(faker.person, 'lastName');
-    const email = vi.spyOn(faker.internet, 'email');
-    
-    afterEach(() => {
-        vi.resetAllMocks();
-    })
-    
-    it('is able to spy on the factory functions', async () => {
-        await factory.create();
-        expect(_id).toHaveBeenCalled();
-        expect(firstName).toHaveBeenCalled();
-        expect(lastName).toHaveBeenCalled();
-        expect(email).toHaveBeenCalled();
-    })
-    
-    
-    it('Does not call factory functions more than once', async () => {
-        await factory.create();
-        expect.soft(_id).toHaveBeenCalledOnce();
-        expect.soft(firstName).toHaveBeenCalledOnce();
-        expect.soft(lastName).toHaveBeenCalledOnce();
-        expect.soft(email).toHaveBeenCalledOnce();
-    })
-    
-    it('has the expected output format', async () =>{
-        const result = await factory.create()
-        expect(result).toEqual(
-            expect.objectContaining({
-                _id: expect.any(String),
-                firstName: expect.any(String),
-                lastName: expect.any(String),
-                email: expect.stringContaining('@'),
-                fullName: expect.stringContaining(result.firstName),
-            })
-        );
+    describe('generator functions', () => {
+        const _id = vi.spyOn(simpleFaker.string, 'uuid');
+        const firstName = vi.spyOn(faker.person, 'firstName');
+        const lastName = vi.spyOn(faker.person, 'lastName');
+        const email = vi.spyOn(faker.internet, 'email');
+        
+        afterEach(() => {
+            vi.resetAllMocks();
+        });
+        
+        afterAll(() => {
+            vi.restoreAllMocks();
+        });
+        
+        it('can be spied on', async () => {
+            await factory.create();
+            expect(_id).toHaveBeenCalled();
+            expect(firstName).toHaveBeenCalled();
+            expect(lastName).toHaveBeenCalled();
+            expect(email).toHaveBeenCalled();
+        })
+        
+        
+        it(`only get called once even when referenced in other properties`, async () => {
+            await factory.create();
+            expect.soft(_id).toHaveBeenCalledOnce();
+            expect.soft(firstName).toHaveBeenCalledOnce();
+            expect.soft(lastName).toHaveBeenCalledOnce();
+            expect.soft(email).toHaveBeenCalledOnce();
+        })
+        
+        it('has the expected output format', async () =>{
+            const result = await factory.create()
+            expect(result).toEqual(
+                expect.objectContaining({
+                    _id: expect.any(String),
+                    firstName: expect.any(String),
+                    lastName: expect.any(String),
+                    email: expect.stringContaining('@'),
+                    fullName: expect.stringContaining(result.firstName),
+                })
+            );
+        })
     })
 });
 
