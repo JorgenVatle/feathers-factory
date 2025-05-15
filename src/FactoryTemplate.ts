f75f35ac9e652522b064d147a0604603987a99b7
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-15 18:45:06 +0300

    Add template field and template context types

--- src/FactoryTemplate.ts
+++ src/FactoryTemplate.ts
@@ -1,3 +1,11 @@
 export class FactoryTemplate<TTemplate> {
     constructor(protected readonly template: TTemplate) {}
 }
+
+export type TemplateContext<TTemplate> = {
+    [key in keyof TTemplate]: TemplateField<TTemplate[key]>;
+}
+
+export type TemplateField<
+    TValue = unknown,
+> = TValue | (() => TValue | Promise<TValue>)
