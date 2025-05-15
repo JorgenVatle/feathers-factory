6b8b89c7e48cb7ac9b9e47addc670f3193e52ace
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-15 19:27:22 +0300

    Add doc blocks to template type aliases

--- src/FactoryTemplate.ts
+++ src/FactoryTemplate.ts
@@ -13,10 +13,28 @@ export type TemplateContext<TTemplate> = {
     [key in keyof TTemplate]: TemplateField<TTemplate[key]>;
 }
 
+/**
+ * Factory Template field.
+ * Specifies a function to run every time the factory is called. Or a static
+ * value that will always remain the same.
+ */
 export type TemplateField<
     TValue = unknown,
 > = TValue | (() => TValue | Promise<TValue>);
 
+/**
+ * Factory Template result.
+ * The raw output of type of the template after resolving all fields.
+ */
 type TemplateResult<TTemplate> = {
     [key in keyof TTemplate]: TTemplate[key] extends TemplateField<infer T> ? T : never;
+}
+
+/**
+ * Template overrides.
+ * Defines the fields that can be overridden before resolving the final
+ * template.
+ */
+type TemplateOverrides<TTemplate> = {
+    [key in keyof TTemplate]?: TemplateField<TTemplate[key]>;
 }
