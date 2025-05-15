4a3d2d835ccdc2677fcd4223a2cec6e198360e26
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-15 23:57:14 +0300

    Use template schema for template context state

--- src/TemplateContext.ts
+++ src/TemplateContext.ts
@@ -5,7 +5,7 @@ export class TemplateContext<TTemplate> {
     protected readonly state: ContextState<TTemplate>;
     
     constructor(protected readonly template: FactoryTemplate<TTemplate>) {
-        this.state = Object.create(template);
+        this.state = Object.create(template._schema);
     }
     
     public get(key: keyof TTemplate) {
