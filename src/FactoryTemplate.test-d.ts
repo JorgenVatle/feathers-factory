e2cb690e8a9d3f00f509e1bf3c70a36edb9f016d
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-15 19:11:18 +0300

    Assert that overrides have to adhere to the original schema type

--- src/FactoryTemplate.test-d.ts
+++ src/FactoryTemplate.test-d.ts
@@ -4,13 +4,14 @@ import { FactoryTemplate } from './FactoryTemplate';
 describe('FactoryTemplate', () => {
     
     
+    const template = new FactoryTemplate({
+        _id: () => 'test',
+        createdAt: () => new Date(),
+        firstName: 'test',
+        lastName: 'test',
+    });
+    
     describe('Resolved data types', async () => {
-        const template = new FactoryTemplate({
-            _id: () => 'test',
-            createdAt: () => new Date(),
-            firstName: 'test',
-            lastName: 'test',
-        });
         const resolvedTemplate = await template.resolve();
         
         it('will unwrap functions to their return values', async () => {
@@ -21,6 +22,15 @@ describe('FactoryTemplate', () => {
         it('will not unwrap non-function values', async () => {
             expectTypeOf(resolvedTemplate.firstName).toEqualTypeOf<string>();
             expectTypeOf(resolvedTemplate.lastName).toEqualTypeOf<string>();
+        })
+        
+    })
+    
+    describe('Overrides', () => {
+        it('has to adhere to the original schema', () => {
+            expectTypeOf(template.resolve).parameter(1).not.toBeCallableWith({
+                _id: () => 'INVALID_ID',
+            })
         })
         
     })
