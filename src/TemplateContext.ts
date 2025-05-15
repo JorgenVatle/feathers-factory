e44caee203474421c423c559f9ea51977ac2c642
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-15 23:45:11 +0300

    Add context field and context state types.

--- src/TemplateContext.ts
+++ src/TemplateContext.ts
@@ -7,7 +7,27 @@ class TemplateContext<TTemplate> {
         this.result = Object.create(template);
     }
     
-    public resolve() {
+    public get(key: keyof TTemplate) {
         // todo
     }
 }
+
+/**
+ * Resolver state. May contain some partially resolved template fields.
+ */
+type ContextState<TTemplate> = {
+    [key in keyof TTemplate]: ContextField<TTemplate[key]>;
+}
+
+/**
+ * Contextualized FactoryTemplate fields.
+ * When resolving some fields will get converted to promises,
+ * some immediately to their resulting value, etc.
+ *
+ * Todo: Attempt to infer types that will never get converted to a promise or
+ *  function. (sync functions, static values, etc.)
+ */
+type ContextField<TType> =
+    | TType
+    | Promise<TType>
+    | (() => Promise<TType> | TType);
