5557c51a725ae64f2ed7a7c60a2ea0562067a6e1
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-16 04:10:44 +0300

    Add namespaced type exports for templates

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,14 @@ export { FactoryService } from './ServiceTypes';
 export { TemplateContext } from './TemplateContext';
 export { FactoryTemplate } from './FactoryTemplate';
 
+import * as TemplateTypes from './FactoryTemplate';
+
 export { default as GlobalFactories } from './GlobalFactories';
+
+export namespace Template {
+    export import InferFieldType = TemplateTypes.InferFieldType;
+    export import InferOutput = TemplateTypes.InferOutput;
+    export import Overrides = TemplateTypes.TemplateOverrides;
+    export import Result = TemplateTypes.TemplateResult;
+    export import Schema = TemplateTypes.TemplateSchema;
+}
