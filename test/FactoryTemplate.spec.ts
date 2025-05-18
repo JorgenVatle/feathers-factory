7e6b5ed9a99081ce3b7677f95ad267134d2d2d1c
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-18 16:46:19 +0300

    Refactor factory template tests to avoid use of unscoped arrow functions

--- test/FactoryTemplate.spec.ts
+++ test/FactoryTemplate.spec.ts
@@ -9,17 +9,23 @@ describe('Real-world examples', () => {
                 {
                     description: 'Test',
                     quantity: 1,
-                    price: 10
+                    price: 10,
                 },
             ],
             taxRate: 0.19,
             async subtotal() {
                 const items = await this.get('items');
-                return items.reduce((acc, item) => acc + item.quantity * item.price, 0)
+                return items.reduce((acc, item) => acc + item.quantity * item.price, 0);
             },
-            tax: async (ctx) => await ctx.get('subtotal') * await ctx.get('taxRate'),
-            total: (ctx) => await ctx.get('subtotal') + await ctx.get('tax'),
-            createdAt: () => new Date(),
+            async tax() {
+                return await this.get('subtotal') * await this.get('taxRate')
+            },
+            async total() {
+                return await this.get('subtotal') + await this.get('tax')
+            },
+            async createdAt(){
+              return new Date();
+            }
         });
         
         it('calculates the correct subtotal', async () => {
@@ -27,5 +33,5 @@ describe('Real-world examples', () => {
         });
         
         it.todo('can use nested functions to generate any number of line items');
-    })
-})
+    });
+});
