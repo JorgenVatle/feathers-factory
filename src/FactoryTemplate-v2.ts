85de14f40d78ed720ce6edb96cc81dd5edc9ee37
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-19 23:25:13 +0300

    Document generic parameters

--- src/FactoryTemplate-v2.ts
+++ src/FactoryTemplate-v2.ts
@@ -2,12 +2,21 @@ import type { Simplify } from 'type-fest';
 
 
 export function defineTemplateSchema<
-    TReturnType extends Record<keyof TOutputType, unknown>,
-    TOutputType extends {
+    /**
+     * The return type for each template function in the template.
+     */
+    TReturnType extends Record<keyof TFieldContext, unknown>,
+    
+    /**
+     * Mapped type of the context to expose to each field respectively.
+     * We need to omit the return type of the current field to prevent
+     * the compiler from squawking and deferring to 'unknown' for everything
+     */
+    TFieldContext extends {
         [key in keyof TReturnType]: Simplify<Omit<TReturnType, key>>;
     },
 >(template: {
-    [key in keyof TReturnType | keyof TOutputType]: (context: TOutputType[key]) => TReturnType[key];
+    [key in keyof TReturnType | keyof TFieldContext]: (context: TFieldContext[key]) => TReturnType[key];
 }) {
     return {};
 }
