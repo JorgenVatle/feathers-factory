87d3fd4f290703d9bbdc097dc30db7d131881c19
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-23 11:08:22 +0300

    Add tests for both param and 'this' signature

--- src/Template/Template.test-d.ts
+++ src/Template/Template.test-d.ts
@@ -54,10 +54,10 @@ describe('FactoryTemplateV2', () => {
             asyncPromise: async () => 'ok' as const,
             asyncDate: () => new Date(),
             
-            async test(ctx) {
-                expectTypeOf(await ctx.get('arrowFunction')).toEqualTypeOf<'ok'>();
-                expectTypeOf(await ctx.get('asyncPromise')).toEqualTypeOf<'ok'>();
-                expectTypeOf(await ctx.get('asyncDate')).toEqualTypeOf<Date>();
+            async testThis() {
+                expectTypeOf(await this.get('arrowFunction')).toEqualTypeOf<'ok'>();
+                expectTypeOf(await this.get('asyncPromise')).toEqualTypeOf<'ok'>();
+                expectTypeOf(await this.get('asyncDate')).toEqualTypeOf<Date>();
                 return 'ok';
             }
         })
@@ -67,12 +67,19 @@ describe('FactoryTemplateV2', () => {
         const template = new FactoryTemplateV2({
             arrowFunction: () => 'ok' as const,
             asyncPromise: async () => 'ok' as const,
-            asyncDate() { return new Date() },
+            asyncDate: () => { return new Date() },
             
-            async test() {
+            async testThis() {
                 expectTypeOf(await this.get('arrowFunction')).toEqualTypeOf<'ok'>();
                 expectTypeOf(await this.get('asyncPromise')).toEqualTypeOf<'ok'>();
                 expectTypeOf(await this.get('asyncDate')).toEqualTypeOf<Date>();
+                return 'ok';
+            },
+            
+            async testParam(ctx) {
+                expectTypeOf(await ctx.get('arrowFunction')).toEqualTypeOf<'ok'>();
+                expectTypeOf(await ctx.get('asyncPromise')).toEqualTypeOf<'ok'>();
+                expectTypeOf(await ctx.get('asyncDate')).toEqualTypeOf<Date>();
                 return 'ok';
             }
         })
@@ -106,7 +113,11 @@ describe('FactoryTemplateV2', () => {
         const template = new FactoryTemplateV2({
             staticField: 'ok' as const,
             staticPromise: Promise.resolve('ok' as const),
-            async test(ctx) {
+            async testThis() {
+                expectTypeOf(await this.get('staticField')).toEqualTypeOf<'ok'>();
+                expectTypeOf(await this.get('staticPromise')).toEqualTypeOf<'ok'>();
+            },
+            async testParam(ctx) {
                 expectTypeOf(await ctx.get('staticField')).toEqualTypeOf<'ok'>();
                 expectTypeOf(await ctx.get('staticPromise')).toEqualTypeOf<'ok'>();
             }
@@ -129,7 +140,15 @@ describe('FactoryTemplateV2', () => {
                     }
                 },
                 
-                async test(ctx) {
+                async testThis() {
+                    expectTypeOf(await this.get('firstName')).toEqualTypeOf<'John'>();
+                    expectTypeOf(await this.get('lastName')).toEqualTypeOf<'Doe'>();
+                    expectTypeOf(await this.get('address.street')).toEqualTypeOf<string>();
+                    expectTypeOf(await this.get('address.city')).toEqualTypeOf<string>();
+                    expectTypeOf(await this.get('address.zip')).toEqualTypeOf<number>();
+                },
+                
+                async testParam(ctx) {
                     expectTypeOf(await ctx.get('firstName')).toEqualTypeOf<'John'>();
                     expectTypeOf(await ctx.get('lastName')).toEqualTypeOf<'Doe'>();
                     expectTypeOf(await ctx.get('address.street')).toEqualTypeOf<string>();
@@ -155,7 +174,13 @@ describe('FactoryTemplateV2', () => {
                     zip: parseInt(faker.location.zipCode()),
                 }),
                 
-                async test(ctx) {
+                async testThis() {
+                    expectTypeOf(await this.get('address.street')).toEqualTypeOf<string>();
+                    expectTypeOf(await this.get('address.city')).toEqualTypeOf<string>();
+                    expectTypeOf(await this.get('address.zip')).toEqualTypeOf<number>();
+                },
+                
+                async testParam(ctx) {
                     expectTypeOf(await ctx.get('address.street')).toEqualTypeOf<string>();
                     expectTypeOf(await ctx.get('address.city')).toEqualTypeOf<string>();
                     expectTypeOf(await ctx.get('address.zip')).toEqualTypeOf<number>();
