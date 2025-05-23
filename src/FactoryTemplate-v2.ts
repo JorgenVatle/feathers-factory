39f967ebac370e44b9909e73240eba2c14054ccb
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-23 05:32:28 +0300

    Move FactoryTemplate v2 context class into Template directory

--- src/FactoryTemplate-v2.ts
+++ src/FactoryTemplate-v2.ts
@@ -1,4 +1,5 @@
-import type { Get, Paths, Simplify } from 'type-fest';
+import type { Simplify } from 'type-fest';
+import type { SchemaContext } from './Template/Context';
 
 
 export function defineTemplateSchema<
@@ -46,27 +47,6 @@ export type TemplateSchema<
 > = {
     [key in keyof TSchema]: TemplateFunction<TSchema[key], TFieldContext[key]> | TSchema[key];
 } & ThisType<SchemaContext<TSchema>>;
-
-/**
- * Unwrap any promises within the provided schema to enable dot notation
- * accessors for nested promisified fields.
- */
-type ResolveSchemaOutput<TSchema> = {
-    [key in keyof TSchema]: TSchema[key] extends Promise<infer T> ? T : TSchema[key];
-}
-
-export abstract class SchemaContext<TSchema, TOutput = ResolveSchemaOutput<TSchema>> {
-    public get<TKey extends Paths<TOutput> & string>(key: TKey): Promise<Get<TOutput, TKey>> {
-        return this._get(key);
-    }
-    
-    public call<TKey extends Paths<TOutput> & string>(key: TKey): Promise<Get<TOutput, TKey>> {
-        return this._call(key);
-    }
-    
-    protected abstract _get(path: string): Promise<any>;
-    protected abstract _call(path: string): Promise<any>;
-}
 
 export class FactoryTemplateV2<
     TSchema extends BaseSchema,
---
+++ src/Template/Context.ts
@@ -0,0 +1,28 @@
+import type { Get, Paths } from 'type-fest';
+
+/**
+ * Factory Template context.
+ * Provides access to the current generation context.
+ *
+ * The `this` type within your template's generator functions.
+ */
+export abstract class SchemaContext<TSchema, TOutput = ResolveSchemaOutput<TSchema>> {
+    public get<TKey extends Paths<TOutput> & string>(key: TKey): Promise<Get<TOutput, TKey>> {
+        return this._get(key);
+    }
+    
+    public call<TKey extends Paths<TOutput> & string>(key: TKey): Promise<Get<TOutput, TKey>> {
+        return this._call(key);
+    }
+    
+    protected abstract _get(path: string): Promise<any>;
+    protected abstract _call(path: string): Promise<any>;
+}
+
+/**
+ * Unwrap any promises within the provided schema to enable dot notation
+ * accessors for nested promisified fields.
+ */
+type ResolveSchemaOutput<TSchema> = {
+    [key in keyof TSchema]: TSchema[key] extends Promise<infer T> ? T : TSchema[key];
+}
