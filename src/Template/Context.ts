438790a34199aee05fae40a3578f23679cab57d8
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-23 07:56:11 +0300

    Accept dot notation path in addition to output key for get() method

--- src/Template/Context.ts
+++ src/Template/Context.ts
@@ -7,8 +7,17 @@ import type { BaseSchema, ResolveSchemaOutput } from './Schema';
  *
  * The `this` type within your template's generator functions.
  */
-export abstract class SchemaContext<TSchema extends BaseSchema, TOutput = ResolveSchemaOutput<TSchema>> {
-    public get<TKey extends Paths<TOutput> & string>(key: TKey): Promise<Get<TOutput, TKey>> {
+export abstract class SchemaContext<
+    TSchema extends BaseSchema,
+    TOutput extends ResolveSchemaOutput<TSchema> = ResolveSchemaOutput<TSchema>,
+    TPaths extends string = Extract<Paths<TOutput>, string>,
+> {
+    public readonly _output!: TOutput;
+    
+    public get<TKey extends keyof TOutput>(key: TKey): Promise<TOutput[TKey]>
+    public get<TKey extends TPaths>(key: TKey): Promise<Get<TOutput, TKey>>
+    public get(key: TPaths | keyof TOutput): Promise<unknown> {
+        // @ts-expect-error Todo: resolve type error
         return this._get(key);
     }
     
