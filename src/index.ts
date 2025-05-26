5f2cbdc5232fe5838d835a8aff7735c151c30684
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-26 22:22:47 +0300

    Remove redundant TemplateTypes module

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { default as Factory } from './Factory';
 export { FactoryService } from './ServiceTypes';
 
-export type * as Template from './TemplateTypes';
-
 export { default as GlobalFactories } from './GlobalFactories';
 export * from './Template';
