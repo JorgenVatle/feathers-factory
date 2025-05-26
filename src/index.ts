4e28376251c7262e66c0a371df8f39c0a4a02525
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-26 22:22:24 +0300

    Re-export everything from Template index

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ export { FactoryService } from './ServiceTypes';
 export type * as Template from './TemplateTypes';
 
 export { default as GlobalFactories } from './GlobalFactories';
-export { FactoryTemplate } from './Template/Template';
-export { TemplateContext } from './Template/Context';
+export * from './Template';
