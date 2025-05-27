4525e11fc87d8ca54b6799075e9388e418d1d86c
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-28 02:00:26 +0300

    Remove one level of nesting for Factory type tests

--- packages/feathers-factory/test/Factory.test-d.ts
+++ packages/feathers-factory/test/Factory.test-d.ts
@@ -3,107 +3,100 @@ import type { FactoryService } from 'feathers-factory';
 import { Factory } from 'feathers-factory';
 import { describe, expectTypeOf, it } from 'vitest';
 
-describe('Factory Types', () => {
+describe('FactoryCompatibleService', () => {
+    function createFactory<
+        TSchema,
+        TOutput = TSchema
+    >(service: FactoryService<TSchema, TOutput>): NoInfer<(data: TSchema) => TOutput> {
+        return {} as any;
+    }
     
-    describe('FactoryCompatibleService', () => {
-        function createFactory<
-            TSchema,
-            TOutput = TSchema
-        >(service: FactoryService<TSchema, TOutput>): NoInfer<(data: TSchema) => TOutput> {
-            return {} as any;
-        }
+    it('is compatible with Feathers services', () => {
+        type Schema = { foo: 'bar' }
+        const service = {} as Service<Schema>;
+        const create = createFactory(service);
         
-        it('is compatible with Feathers services', () => {
-            type Schema = { foo: 'bar' }
-            const service = {} as Service<Schema>;
-            const create = createFactory(service);
-            
-            expectTypeOf(create)
-                .parameter(0)
-                .toEqualTypeOf<Partial<Schema>>();
+        expectTypeOf(create)
+            .parameter(0)
+            .toEqualTypeOf<Partial<Schema>>();
+    })
+    
+})
+
+it('can infer types from a generic Feathers service', async () => {
+    const service = {} as Service<{ foo: 'bar' }>;
+    const myFactory = new Factory(service, {} as any)
+    
+    expectTypeOf(myFactory.create()).resolves.toEqualTypeOf<{ foo: 'bar' }>();
+});
+
+it('can fall back to the service create() data type for non-Feathers services', async () => {
+    const service = {
+        async create(data: { foo: 'bar' }) { return data }
+    };
+    const myFactory = new Factory(service, {} as any);
+    
+    expectTypeOf(myFactory.create()).resolves.toEqualTypeOf<{ foo: 'bar' }>();
+})
+
+it(`disallows generator types that don't match the underlying schema `, () => {
+    const service = {} as Service<{
+        stringField: string,
+        numberField: number,
+    }>;
+    
+    new Factory(service, {
+        // @ts-expect-error
+        stringField: 1,
+        // @ts-expect-error
+        numberField: () => 'foobar'
+    });
+});
+
+describe('Service params', () => {
+    describe('Non-standard Service params', () => {
+        const factory = new Factory({
+            create(
+                data: { foo: 'bar' },
+                params: { custom: 'param' }
+            ) {}
+        }, { foo: 'bar', });
+        
+        it('allows expected params to be passed', () => {
+            expectTypeOf(factory.create).toBeCallableWith({}, { custom: 'param' })
         })
         
+        it('forbids unexpected params to be passed', () => {
+            expectTypeOf(factory.create).toBeCallableWith({}, {
+                // @ts-expect-error
+                custom: 'param1'
+            })
+        })
     })
     
-    it('can infer types from a generic Feathers service', async () => {
-        const service = {} as Service<{ foo: 'bar' }>;
-        const myFactory = new Factory(service, {} as any)
+    describe('Standard Feathers Service params', () => {
+        type DocumentType = { foo: 'bar' };
+        const factory = new Factory({} as Service<DocumentType, DocumentType, Params<DocumentType>>, { foo: 'bar', });
         
-        expectTypeOf(myFactory.create()).resolves
-            .toEqualTypeOf<{ foo: 'bar' }>();
-    });
-    
-    it('can fall back to the service create() data type for non-Feathers services', async () => {
-        const service = {
-            async create(data: { foo: 'bar' }) { return data }
-        };
-        const myFactory = new Factory(service, {} as any);
-        
-        expectTypeOf(myFactory.create()).resolves.toEqualTypeOf<{ foo: 'bar' }>();
-    })
-    
-    it(`disallows generator types that don't match the underlying schema `, () => {
-        const service = {} as Service<{
-            stringField: string,
-            numberField: number,
-        }>;
-        
-        new Factory(service, {
-            // @ts-expect-error
-            stringField: 1,
-            // @ts-expect-error
-            numberField: () => 'foobar'
+        it('allows expected params to be passed', () => {
+            expectTypeOf(factory.create).toBeCallableWith({}, { paginate: false })
+            expectTypeOf(factory.create).toBeCallableWith({}, { paginate: { max: 10 } })
         });
-    });
-    
-    
-    describe('Service params', () => {
-        describe('Non-standard Service params', () => {
-            const factory = new Factory({
-                create(
-                    data: { foo: 'bar' },
-                    params: { custom: 'param' }
-                ) {}
-            }, { foo: 'bar', });
-            
-            it('allows expected params to be passed', () => {
-                expectTypeOf(factory.create).toBeCallableWith({}, { custom: 'param' })
-            })
-            
-            it('forbids unexpected params to be passed', () => {
-                expectTypeOf(factory.create).toBeCallableWith({}, {
-                    // @ts-expect-error
-                    custom: 'param1'
-                })
-            })
-        })
         
-        describe('Standard Feathers Service params', () => {
-            type DocumentType = { foo: 'bar' };
-            const factory = new Factory({} as Service<DocumentType, DocumentType, Params<DocumentType>>, { foo: 'bar', });
-            
-            it('allows expected params to be passed', () => {
-                expectTypeOf(factory.create).toBeCallableWith({}, { paginate: false })
-                expectTypeOf(factory.create).toBeCallableWith({}, { paginate: { max: 10 } })
+        it('forbids unexpected params to be passed', () => {
+            expectTypeOf(factory.create).toBeCallableWith({}, {
+                query: {
+                    // @ts-expect-error
+                    foo: 'invalid',
+                }
             });
             
-            it('forbids unexpected params to be passed', () => {
-                expectTypeOf(factory.create).toBeCallableWith({}, {
-                    query: {
-                        // @ts-expect-error
-                        foo: 'invalid',
-                    }
-                });
-                
-                expectTypeOf(factory.create).toBeCallableWith({}, {
-                    query: {
-                        // @ts-expect-error
-                        unknownField: 'invalid',
-                    }
-                })
+            expectTypeOf(factory.create).toBeCallableWith({}, {
+                query: {
+                    // @ts-expect-error
+                    unknownField: 'invalid',
+                }
             })
         })
     })
-    
-    
 })
