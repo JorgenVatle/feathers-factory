ce0621f3847345c5fcc29dad1f8a1dbab34ec7a7
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-27 20:42:19 +0300

    Re-export contents of Factory directory in main index module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { default as Factory } from './Factory';
-export { FactoryService } from './ServiceTypes';
-
 export { default as GlobalFactories } from './Factory/GlobalFactories';
+export * from './Factory';
 export * from './Template';
