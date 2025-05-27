c88f2bca016fc797cb4e6699ec4c1522fdc9ea97
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-27 22:57:17 +0300

    Add alias for feathers-factory to test config

--- examples/_basic-feathers-app/vitest.config.ts
+++ examples/_basic-feathers-app/vitest.config.ts
@@ -1,7 +1,13 @@
+import * as Path from 'node:path';
 import { defineProject } from 'vitest/config';
+
+const __dirname = new URL('.', import.meta.url).pathname;
 
 export default defineProject({
     test: {
-        name: 'Basic Feathers App'
+        name: 'Basic Feathers App',
+        alias: {
+            'feathers-factory': Path.join(__dirname, '../../src/index.ts'),
+        },
     }
 })
