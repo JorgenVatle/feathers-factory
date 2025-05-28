854f0242a134ea045d6c92a67e5a21e9237ff447
Jørgen Vatle <jorgen@wcaservices.net>
2025-05-28 05:02:49 +0300

    Move context param in TemplaceSchema before schema2 param

--- packages/feathers-factory/src/Template/Schema.ts
+++ packages/feathers-factory/src/Template/Schema.ts
@@ -8,8 +8,8 @@ import type { SchemaContext } from './Context';
  */
 export type TemplateSchema<
     TSchema,
-    TSchema2 = TSchema,
     TContext = SchemaContext<TSchema>,
+    TSchema2 = TSchema,
 > = & ExtendTemplateSchema<TSchema>
     & ThisType<TContext>
     & { [key in keyof TSchema2 as key extends keyof TSchema ? never : key]: SchemaField<TSchema2[key], TContext> };
